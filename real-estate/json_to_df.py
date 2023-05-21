0c80f4238459fd2c62799ca137086a50021f8b7d
Stelios Antorkas <72335516+santorkas12@users.noreply.github.com>
2023-05-21 17:41:49 +0300

    Refactor CSV write file into dedicated class

--- real-estate/json_to_df.py
+++ real-estate/json_to_df.py
@@ -5,21 +5,22 @@ import pytz
 import os
 pd.set_option('display.max_columns', None)
 
-if __name__ == '__main__':
+class CSVWriter:
     current_time = datetime.datetime.now(pytz.timezone('Europe/Nicosia')).strftime('%m-%d-%Y_%H-%M-%S')
 
-    with open('property_listings.json', 'r') as f:
-        property_listings = json.load(f)
+    def write_to_csv(self):
+        with open('property_listings.json', 'r') as f:
+            property_listings = json.load(f)
 
 
-    sample_dataframe = pd.DataFrame(property_listings)
+        sample_dataframe = pd.DataFrame(property_listings)
 
-    list_of_property_chars = sample_dataframe['property_characteristics'].to_list()
-    list_of_ids = sample_dataframe['id'].to_list()
+        list_of_property_chars = sample_dataframe['property_characteristics'].to_list()
+        list_of_ids = sample_dataframe['id'].to_list()
 
-    property_characteristics = pd.DataFrame(list_of_property_chars)
+        property_characteristics = pd.DataFrame(list_of_property_chars)
 
-    listings_dataframe = pd.concat([sample_dataframe, property_characteristics], axis=1)
-    listings_dataframe.drop('property_characteristics', axis=1, inplace=True)
-    print(os.getcwd())
-    listings_dataframe.to_csv(f'listings/listings_{current_time}.csv')
+        listings_dataframe = pd.concat([sample_dataframe, property_characteristics], axis=1)
+        listings_dataframe.drop('property_characteristics', axis=1, inplace=True)
+        listings_dataframe.to_csv(f'listings/listings_{self.current_time}.csv')
+        print(f'Successfully exported results to listings/listings_{self.current_time}.csv')
